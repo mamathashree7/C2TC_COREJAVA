f79b5cd0a6d87580938d43054957d77b67d92e8d
Shankar <Shankar@DESKTOP-IB3GBB9>
2022-10-17 00:18:48 -0700

    second commit

---
+++ src/IfExample/IfExample.java
@@ -0,0 +1,23 @@
+package IfExample;
+
+public class IfExample {
+
+	public static void main(String[] args) {
+		int a=2;
+		if(a==1) {
+			System.out.println("one");
+		}
+		else if(a==2) {
+			System.out.println("Two");
+		}
+		else if(a==3) {
+			System.out.println("Three");
+		}
+			else {
+			System.out.println("invalid");
+			}
+	}
+}
+	
+
+
---
+++ src/RelationalLogicalOperator/RelationalLogicalOperator.java
@@ -0,0 +1,22 @@
+package RelationalLogicalOperator;
+
+import java.util.Random;
+
+public class RelationalLogicalOperator {
+
+	public static void main(String[] args) {
+		Random rand =new Random();
+		 int i =rand.nextInt(100);
+		 int j = rand.nextInt(100);
+		 System.out.println("i ="+ i);
+		 System.out.println("j=" + j);
+		 System.out.println((i<10 || (j<10)));
+		 System.out.println(i<=j);
+		 
+		
+		
+		
+
+	}
+
+}
